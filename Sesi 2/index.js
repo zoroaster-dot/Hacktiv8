1da08530ad59b5e8118eb6561ee60acb4ed30f5d
Ifka Nabilah <85624063+zoroaster-dot@users.noreply.github.com>
2022-08-23 22:59:13 +0700

    Create index.js

---
+++ Sesi 2/index.js
@@ -0,0 +1,78 @@
+const animals = [{
+        name: 'Mujair',
+        type: 'fish'
+    },
+    {
+        name: 'Loki',
+        type: 'cat'
+    },
+    {
+        name: 'Nemo',
+        type: 'fish'
+    },
+    {
+        name: 'Jerry',
+        type: 'cat'
+    },
+    {
+        name: 'Tom',
+        type: 'dog'
+    },
+    {
+        name: 'Spike',
+        type: 'dog'
+    },
+    {
+        name: 'Tanuki',
+        type: 'musang'
+    }
+]
+
+
+// expected return 
+// data = {
+//   fish: [
+//     {
+//       name: 'Mujair',
+//       type: 'fish'
+//     },
+//     {
+//       name: 'Nemo',
+//       type: 'fish'
+//     },
+//   ],
+//   cat: [
+//     {
+//       name: 'Jerry',
+//       type: 'cat'
+//     },
+//     {
+//       name: 'Loki',
+//       type: 'cat'
+//     },
+//   ],
+//   dog: [
+//     {
+//       name: 'Tom',
+//       type: 'dog'
+//     },
+//     {
+//       name: 'Spike',
+//       type: 'dog'
+//     }
+//   ]
+// }
+
+const getObjectBeingCategorized = (animals) => {
+    // put logic here
+    const getObjectBeingCategorized = (animals)
+    const data = {};
+    for (const animal of animals) {
+        if (!data.hasOwnProperty(animal.type))
+            data[animal.type] = [animal]
+        else data[animal.type].push(animal);
+    }
+    return data;
+}
+
+console.log(getObjectBeingCategorized(animals))
